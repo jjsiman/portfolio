73eec98b00bf4f94dd3f011ad6f188221a2aea52
Jack Siman <jack.siman@icloud.com>
2021-02-10 09:18:44 -0500

    Reorganizing animations - Added fadeUpIn to contact page

--- apps/personal-website/src/app/pages/home/home.component.ts
+++ apps/personal-website/src/app/pages/home/home.component.ts
@@ -1,25 +1,12 @@
 import { style, transition, trigger, animate, group } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { fadeUpInAnimation } from '../../shared/animations/fadeUpIn';
 
 @Component({
   selector: 'portfolio-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('fadeUpIn', [
-      transition(':enter', [
-        style({ transform: 'translateY(50%)', opacity: 0 }),
-        group([
-          animate('1s ease-out',
-            style({ transform: 'translateY(0%)' }),
-          ),
-          animate('600ms 400ms ease-out',
-            style({ opacity: 1 })
-          )
-        ])
-      ])
-    ])
-  ]
+  animations: [fadeUpInAnimation]
 })
 export class HomeComponent implements OnInit {
 
